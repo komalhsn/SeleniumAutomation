6f4762dd06af7449175c19763df2625b54bd2a08
Komal <komal.srinu1@gmail.com>
2018-12-29 16:47:19 +0530

    Dec28th assigment

---
+++ PythonBasics/Dec_28th/Dictonary_with_key_as_1st_character_and_value_as_words_starting_with_that_character.py
@@ -0,0 +1,4 @@
+#10) wap to create a dictonary with key as 1st character and value as words starting with that character
+l=['a','b','c','d']
+l2=['bat','dog','apple','cat']
+d={ }
---
+++ PythonBasics/Dec_28th/GenarateRandomNums.py
@@ -0,0 +1,5 @@
+#8 wap to genrate random numbers
+
+import random
+for i in range(5):
+    print(random.randint(100,500))
