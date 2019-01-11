d11fdb989850a454a6f2fe504d6d0fea67cc197b
Komal <komal.srinu1@gmail.com>
2019-01-11 17:24:17 +0530

    added new files

---
+++ PythonBasics/Jan_4th_2019/Default_Argument_Values/Default_Argument_Values_EmptyList.py
@@ -0,0 +1,17 @@
+# def func(a, l=[]):
+#     l.append(a)
+#     return l
+#
+#
+# print(func(1))
+# print(func(2))
+# print(func(3))
+# print(func('a'))
+# print(func('b'))
+
+
+
+def func(l=[]):
+    print(l)
+
+func()
---
+++ PythonBasics/Jan_4th_2019/Default_Argument_Values/Default_Argument_Values_none.py
@@ -0,0 +1,19 @@
+# def f(a, L =None):
+#     if L is None:
+#         L = []
+#     L.append(a)
+#     return L
+#
+#
+# print(f(1))
+# print(f(2))
+# print(f(3))
+# print(f(4))
+
+
+def f(l=None):
+    print(l)
+
+
+f()
+
